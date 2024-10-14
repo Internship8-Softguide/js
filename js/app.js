a5bd514d1e5a25f2bb6313e56eeb5a4490fe56e1
Saw Theodore <sawtheodore@gmail.com>
2024-10-14 12:11:17 +0630

    loop

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 
+// let x = 10;
+
+// while (x < 10) {
+//     console.log("doing..............")
+//     console.log(x)
+//     x++;
+// }
+
+let y = 0
+do {
+    console.log("doing .................")
+    console.log(y);
+    y++;
+} while (y < 10);
+
+console.log("latest condition")
+
+
+
+
+
+
