66aaf4630a226b9ecc1162b6e3d9b68f723d91d9
Saw Theodore <sawtheodore@gmail.com>
2024-10-14 11:16:22 +0630

    fetch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
+const getData = async () => {
 
+    await fetch("https://jsonplaceholder.typicode.com/photos").then((val) => val.json()
+    ).then((jsonValue) => console.log(jsonValue))
+}
+getData();
+console.log("latest")
